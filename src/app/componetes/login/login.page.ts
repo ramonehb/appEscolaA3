512a3a6eb6952c5afbf9448128cdfd860ab58131
Humberto Ramone <humbertorody@gmail.com>
2023-03-30 01:00:10 -0300

    feat: incluindo reactiveForms no login

--- src/app/componetes/login/login.page.ts
+++ src/app/componetes/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 import { AuthService } from '../../services/Auth.service';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,26 +10,62 @@ import { AuthService } from '../../services/Auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  login: string = "";
-  senha: string  = "";
+  formLogin: FormGroup;
+  mensagens = {
+    login: [
+      {tipo: 'required', mensagem: 'Login é obrigatório.'},
+      {tipo: 'minLength', mensagem: 'Login deve ter no mínimo 2 caracter'},
+      {tipo: 'maxLength', mensagem: 'Login deve ter no máximo 15 caracter'}
+    ],
+    senha: [
+      {tipo: 'required', mensagem: 'Senha é obrigatório.'},
+      {tipo: 'minLength', mensagem: 'Senha deve ter no mínimo 2 caracter'},
+      {tipo: 'maxLength', mensagem: 'Senha deve ter no máximo 15 caracter'}
+    ]
+  };
 
-  constructor(private auth: AuthService, private router: Router, private alertController: AlertController) { }
+  constructor(private auth: AuthService, private router: Router, private alertController: AlertController, private toast: ToastController, private formBuilder: FormBuilder) {
+    this.formLogin = this.formBuilder.group({
+      login: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(15)])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(15)])]
+    });
+  }
 
   ngOnInit() {
   }
 
-  async validaLogin() {
-    if (this.auth.validateLogin(this.login, this.senha)) {
+  validaLogin() {
+    if (this.auth.validateLogin(this.formLogin.get('login')?.value, this.formLogin.get('senha')?.value)) {
       this.router.navigate(['/folder/Aulas']);
+      this.toastAlert(`Bem vindo ${this.formLogin.get('login')?.value}`, 'success')
     } else {
-      const alert = await this.alertController.create({
+      this.toastAlert('Erro login/senha inválido', 'danger')
+    }
+  }
+
+  async toastAlert(mensagem: string, cor: string) {
+    const toast = await this.toast.create({
+      message: mensagem,
+      color: cor,
+      duration: 2000
+    });
+
+    toast.present();
+  }
+
+  get f(): any {
+    return this.formLogin.controls;
+  }
+
+  public formValidator(campo: FormControl | AbstractControl): any {
+    return { 'is-invalid': campo.errors && campo.touched};
+  }
+}
+/*const alert = await this.alertController.create({
         header: 'Atenção',
         message: 'Usuário/Senha inválido',
         buttons: ['OK'],
       });
 
       await alert.present();
-    }
-  }
-
-}
+      */
